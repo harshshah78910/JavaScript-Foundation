63c37fae8a38e0578b850a8c6cd915bb7f1fcbcb
harshshah78910 <58108585+harshshah78910@users.noreply.github.com>
2019-11-24 11:06:29 +0530

    switch Example
    Switch Example JavaScript

---
+++ swtichEx.js
@@ -0,0 +1,16 @@
+// switch example 
+
+var game='NFS';
+
+switch(game)
+{
+   case 'Mario' :
+        console.log('This is Mario');
+        break;
+    case 'NFS' : 
+        console.log('This is NFS most Wanted ');
+        break;
+    default : 
+        console.log('You are playing something else');
+   
+}
